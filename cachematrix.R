ec295de58053a1e66ddec63cc26db7a2596c213a
APansirus <APansirus@users.noreply.github.com>
2017-08-22 14:58:42 +0200

    cachematrix changed
    placed my solution in makeCacheMatrix and cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions cache a matrix and compute and cache its inverse
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache itself and 
+## its inverse. 
+## If I understand the subfunctions taken from the vector example correctly:
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    # set assigns its argument to the argument of the parent function and sets 
+    # the variable for the inverse back to NULL (it deletes the old inverse)
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    
+    # get returns the argument of the parent function
+    get <- function() x
+    
+    # setInverse caches its argument to the inv-variable from the parent function
+    setInverse <- function(inverseM) inv <<- inverseM
+    
+    # getInverse returns the inv-variable from the parent function
+    getInverse <- function() inv
+    list(set = set, get = get,
+        setInverse = setInverse,
+        getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by 
+## makeCacheMatrix above. If the inverse has already been calculated for this 
+## matrix, then the cacheSolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getInverse()
+    # if there's no value for the inverse cached...
+    if(!is.null(inv)) {
+        message("retrieving the inverse from the cache")
+        return(inv)
+    }
+    
+    # ... cacheSolve gets the matrix from makeCacheMatrix...
+    data <- x$get()
+    
+    # ... computes its inverse ...
+    inv <- solve(data, ...)
+    
+    # ... caches the inverse
+    x$setInverse(inv)
+    
+    # and then invisibly returns the result
+    invisible(inv)
 }
